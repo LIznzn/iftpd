a8685d5af755da10cff8bda9e4a6218768ac5f92
LIznzn <ww@acgmiao.com>
2024-10-30 00:51:09 +0800

    add main window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use gtk::prelude::*;
+
+fn build_ui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+
+    window.set_title("My Rusty Program");
+    window.set_border_width(10);
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(300, 300);
+
+    let button = gtk::Button::with_label("Click me!");
+    window.add(&button);
+
+    window.show_all();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let application = gtk::Application::new(Some("com.github.my.rs"), Default::default());
+    application.connect_activate(build_ui);
+    application.run();
+    // println!("Hello, world!");
 }
