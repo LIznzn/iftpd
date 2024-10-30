09adf3232168c680c45dfcdd7d193ff33bb956e0
LIznzn <ww@acgmiao.com>
2024-10-30 15:11:57 +0800

    update ui

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use gtk::prelude::*;
-use gtk::{ApplicationWindow, Builder};
+use gtk::{ApplicationWindow, Builder, MenuItem};
 
 fn main() {
     let application = gtk::Application::new(
-        Some("com.github.gtk-rs.examples.builder_basics"),
+        Some("com.starmoe.dev.iftpd.application"),
         Default::default(),
     );
-
     application.connect_activate(build_ui);
-
     application.run();
 }
 
@@ -16,8 +14,13 @@ fn build_ui(application: &gtk::Application) {
     let glade_src = include_str!("iftpd-ui.glade");
     let builder = Builder::from_string(glade_src);
 
-    let window: ApplicationWindow = builder.object("MainWindow").expect("Couldn't get MainWindow");
-    window.set_application(Some(application));
+    let window_main: ApplicationWindow = builder.object("window_main").expect("Couldn't get window_main");
+    window_main.set_application(Some(application));
 
-    window.show_all();
+    let menu_about: MenuItem = builder.object("menu_about").expect("Couldn't get menu_about");
+    menu_about.connect_activate(|_| {
+        println!("触发关于弹窗！")
+    });
+
+    window_main.show_all();
 }
